3a51fb6f3e1e0100e0eb876da1b4b73e45cb3625
ankeshanand <ankeshanand1994@gmail.com>
2014-05-20 00:47:17 +0530

    Add the plots app, update models.py in plots/ to integrate from the legacy database. Add gitignore. Add some bootstrap CSS files. Update settings to include plots app and update database settings to include benchmark_db

---
+++ plots/models.py
@@ -0,0 +1,131 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#     * Rearrange models' order
+#     * Make sure each model has one field with primary_key=True
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
+# into your database.
+
+from django.db import models
+
+
+class BenchmarkLogs(models.Model):
+    id = models.IntegerField(primary_key=True)
+    machine_desc = models.CharField(max_length=765, blank=True)
+    brlcad_version = models.CharField(max_length=45, blank=True)
+    running_time = models.IntegerField(null=True, blank=True)
+    time_of_execution = models.DateTimeField()
+    approx_vgr = models.FloatField(null=True, blank=True)
+    log_vgr = models.FloatField(null=True, blank=True)
+    params = models.CharField(max_length=765, blank=True)
+    results = models.CharField(max_length=765, blank=True)
+    compiler_flags = models.TextField(blank=True)
+    complete_info = models.TextField(blank=True)
+    class Meta:
+        db_table = u'benchmark_logs'
+
+
+class EmailLogs(models.Model):
+    imap_id = models.IntegerField(null=True, blank=True)
+    md5 = models.CharField(max_length=300, blank=True)
+    time = models.DateTimeField()
+    class Meta:
+        db_table = u'email_logs'
+
+
+class EmailVerificationLogs(models.Model):
+    verified_till = models.IntegerField(null=True, blank=True)
+    time = models.DateTimeField()
+    class Meta:
+        db_table = u'email_verification_logs'
+
+
+class MachineInfo(models.Model):
+    benchmark = models.ForeignKey(BenchmarkLogs, null=True, blank=True)
+    osrelease = models.CharField(max_length=300, blank=True)
+    cpu_mhz = models.FloatField(null=True, blank=True)
+    hostname = models.CharField(max_length=300, blank=True)
+    cores = models.IntegerField(null=True, blank=True)
+    processors = models.IntegerField(null=True, blank=True)
+    physical_addr_size = models.IntegerField(null=True, blank=True)
+    virtual_addr_size = models.IntegerField(null=True, blank=True)
+    vendor_id = models.CharField(max_length=300, blank=True)
+    ostype = models.CharField(max_length=300, blank=True)
+    model_name = models.CharField(max_length=300, blank=True)
+    class Meta:
+        db_table = u'machine_info'
+
+
+class Md5Log(models.Model):
+    benchmark = models.ForeignKey(BenchmarkLogs, null=True, blank=True)
+    file_name = models.CharField(max_length=765, blank=True)
+    md5sum = models.CharField(max_length=300, blank=True)
+    archived = models.CharField(max_length=9, blank=True)
+    db_entries = models.CharField(max_length=9, blank=True)
+    class Meta:
+        db_table = u'md5_log'
+
+
+class RtAverage(models.Model):
+    benchmark = models.ForeignKey(BenchmarkLogs, null=True, blank=True)
+    abs_rps = models.FloatField(null=True, blank=True)
+    vgr_rps = models.FloatField(null=True, blank=True)
+    result = models.CharField(max_length=57, blank=True)
+    class Meta:
+        db_table = u'rt_average'
+
+
+class RtBldg391(models.Model):
+    benchmark = models.ForeignKey(BenchmarkLogs, null=True, blank=True)
+    abs_rps = models.FloatField(null=True, blank=True)
+    vgr_rps = models.FloatField(null=True, blank=True)
+    result = models.CharField(max_length=57, blank=True)
+    class Meta:
+        db_table = u'rt_bldg391'
+
+
+class RtM35(models.Model):
+    benchmark = models.ForeignKey(BenchmarkLogs, null=True, blank=True)
+    abs_rps = models.FloatField(null=True, blank=True)
+    vgr_rps = models.FloatField(null=True, blank=True)
+    result = models.CharField(max_length=57, blank=True)
+    class Meta:
+        db_table = u'rt_m35'
+
+
+class RtMoss(models.Model):
+    benchmark = models.ForeignKey(BenchmarkLogs, null=True, blank=True)
+    abs_rps = models.FloatField(null=True, blank=True)
+    vgr_rps = models.FloatField(null=True, blank=True)
+    result = models.CharField(max_length=57, blank=True)
+    class Meta:
+        db_table = u'rt_moss'
+
+
+class RtSphflake(models.Model):
+    benchmark = models.ForeignKey(BenchmarkLogs, null=True, blank=True)
+    abs_rps = models.FloatField(null=True, blank=True)
+    vgr_rps = models.FloatField(null=True, blank=True)
+    result = models.CharField(max_length=57, blank=True)
+    class Meta:
+        db_table = u'rt_sphflake'
+
+
+class RtStar(models.Model):
+    benchmark = models.ForeignKey(BenchmarkLogs, null=True, blank=True)
+    abs_rps = models.FloatField(null=True, blank=True)
+    vgr_rps = models.FloatField(null=True, blank=True)
+    result = models.CharField(max_length=57, blank=True)
+    class Meta:
+        db_table = u'rt_star'
+
+
+class RtWorld(models.Model):
+    benchmark = models.ForeignKey(BenchmarkLogs, null=True, blank=True)
+    abs_rps = models.FloatField(null=True, blank=True)
+    vgr_rps = models.FloatField(null=True, blank=True)
+    result = models.CharField(max_length=57, blank=True)
+    class Meta:
+        db_table = u'rt_world'
+
--- settings.py
+++ settings.py
@@ -13,9 +13,17 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(SITE_ROOT, 'db'), # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(SITE_ROOT, 'db'),  # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    },
+    'benchmark_db': {
+        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'benchmark_db',  # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -102,7 +110,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'django-jquery-file-upload.urls'
+ROOT_URLCONF = 'benchmark.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -118,6 +126,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fileupload',
+    'plots',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
