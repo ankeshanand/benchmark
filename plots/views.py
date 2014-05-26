a8b1b23fb80ca989e282c5879d2d587118a54c82
ankeshanand <ankeshanand1994@gmail.com>
2014-05-26 23:58:07 +0530

    Add two new views corresponding to the new URL mappings

--- plots/views.py
+++ plots/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.utils import simplejson
+from django.http import HttpResponse, Http404
+from .models import BenchmarkLogs, MachineInfo
+
+def rawdata(request, type):
+    if type == "AvgVGRvsProcessor":
+
+
+def draw(request, type):
+    type_dict = {'type': type}
+    return render_to_response("chart.html", type_dict, context_instance=RequestContext(request))
