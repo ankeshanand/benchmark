537fbbf04ad009096595fc940acbbc6d0a2ada78
ankeshanand <ankeshanand1994@gmail.com>
2014-05-27 00:23:50 +0530

    Needed a basic/plus/ mapping as during the POST request while uploading files was generating a 404 error. Also removed /new/ mapping as it's not being used. The file also contains various unused imports, which must be cleaned up later.

--- fileupload/urls.py
+++ fileupload/urls.py
@@ -7,11 +7,7 @@ from fileupload.views import (
         )
 
 urlpatterns = patterns('',
-    #url(r'^basic/$', BasicVersionCreateView.as_view(), name='upload-basic'),
-    url(r'^new/$', BasicPlusVersionCreateView.as_view(), name='upload-basic-plus'),
-    #url(r'^new/$', PictureCreateView.as_view(), name='upload-new'),
-    #url(r'^angular/$', AngularVersionCreateView.as_view(), name='upload-angular'),
-    #url(r'^jquery-ui/$', jQueryVersionCreateView.as_view(), name='upload-jquery'),
+    url(r'^basic/plus/$', BasicPlusVersionCreateView.as_view(), name='upload-basic-plus'),
     url(r'^delete/(?P<pk>\d+)$', PictureDeleteView.as_view(), name='upload-delete'),
     url(r'^view/$', PictureListView.as_view(), name='upload-view'),
 )
