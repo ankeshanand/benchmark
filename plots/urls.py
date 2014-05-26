45896958badb2ff5f7c36a86a60fbdab80d2f618
ankeshanand <ankeshanand1994@gmail.com>
2014-05-26 23:57:11 +0530

    Add the URL mappings for the plots app.

--- plots/urls.py
+++ plots/urls.py
@@ -1,2 +1,8 @@
 __author__ = 'ankesh'
 from django.conf.urls import patterns, url
+from plots.views import rawdata, draw
+
+urlpatterns = patterns('',
+    url(r'^(?P<type>[A-z]+)/$', draw, name='drawChart'),
+    url(r'^(?P<type>[A-z]+)/data/$', rawdata, name='rawdata'),
+)
