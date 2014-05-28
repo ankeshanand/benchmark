c4198bf30b854f48db41ec839f9b9559a61aac4e
ankeshanand <ankeshanand1994@gmail.com>
2014-05-29 01:25:21 +0530

    Add doc strings for methods in plots/views.py

--- plots/views.py
+++ plots/views.py
@@ -9,6 +9,10 @@ import data
 
 
 def rawdata(request, plotname):
+    """
+    Based on the ajax request from the template, it calls data.py to fetch the appropriate data.
+    It returns a JSON dump of the dictionary that is returned from data.py
+    """
     if request.is_ajax():
         try:
             data_dict = {}
@@ -21,5 +25,9 @@ def rawdata(request, plotname):
 
 
 def draw(request, plotname):
+    """
+    The draw view is responsible for drawing the chart. It renders a template chart.html which sends ajax request for the JSON data.
+    It also provides the template the name of the plot to draw via the <name_dict> dictionary.
+    """
     name_dict = {'plotname': plotname}
     return render_to_response("chart.html", name_dict, context_instance=RequestContext(request))
