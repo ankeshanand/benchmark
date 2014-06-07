75f091e284897c3faae15e30ff0a333bb9071901
ankeshanand <ankeshanand1994@gmail.com>
2014-06-07 15:52:00 +0530

    Add a URL pattern for the Plots index page.

--- plots/urls.py
+++ plots/urls.py
@@ -1,9 +1,11 @@
 __author__ = 'ankesh'
 import views
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 #from plots.views import rawdata, draw
 
 urlpatterns = patterns('',
     url(r'^(?P<plotname>[A-z]+)/$', views.draw, name='drawChart'),
     url(r'^(?P<plotname>[A-z]+)/data/$', views.rawdata, name='rawdata'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'))
 )
