5a2673366224751e675b894c13a2152c50d28e87
ankeshanand <ankeshanand1994@gmail.com>
2014-06-11 02:15:16 +0530

    Update to redirect /upload/ to /upload/basic/plus/

--- fileupload/urls.py
+++ fileupload/urls.py
@@ -5,8 +5,10 @@ from fileupload.views import (
         jQueryVersionCreateView, AngularVersionCreateView,
         PictureCreateView, PictureDeleteView, PictureListView,
         )
+from django.http import HttpResponseRedirect
 
 urlpatterns = patterns('',
+    url(r'^$', lambda x: HttpResponseRedirect('/upload/basic/plus/')),
     url(r'^basic/plus/$', BasicPlusVersionCreateView.as_view(), name='upload-basic-plus'),
     url(r'^delete/(?P<pk>\d+)$', PictureDeleteView.as_view(), name='upload-delete'),
     url(r'^view/$', PictureListView.as_view(), name='upload-view'),
