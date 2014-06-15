3c03c792a3c616872a0beb0cfa3787f10100cafd
ankeshanand <ankeshanand1994@gmail.com>
2014-06-16 04:38:43 +0530

    Add a new plot - Absolute Rays Per Sec against Reference Images
    Modifications include necessary addition of the data agrregation code in data.py, fixing link in index.html, and adding support for multi-series charts in flot-chat.html. Also added comments to the index file

--- plots/data.py
+++ plots/data.py
@@ -12,7 +12,7 @@ def avgVGRvsProcessorFamily():
                  'xaxis_title': "Processor Family",
                  'yaxis_title': "Average VGR Rating",
                  'categories': [],
-                 'series-type': "single",
+                 'series_type': "single",
                  'values': []}
 
     distinct_categories_dict = MachineInfo.objects.values('vendor_id').distinct()
@@ -34,7 +34,7 @@ def avgVGRvsOSType():
                  'yaxis_title': "Average VGR Rating",
                  'xaxis_title': "Operating System Type",
                  'categories': [],
-                 'series-type': "single",
+                 'series_type': "single",
                  'values': []}
     distinct_categories_dict = MachineInfo.objects.values('ostype').distinct()
     for category in distinct_categories_dict:
@@ -55,7 +55,7 @@ def avgVGRvsNCores():
                  'yaxis_title': "Average VGR Rating",
                  'xaxis_title': "Number of CPUs",
                  'categories': [],
-                 'series-type': "single",
+                 'series_type': "single",
                  'values': []}
     distinct_number_dict = MachineInfo.objects.values('cores').distinct()
     for number in distinct_number_dict:
@@ -76,7 +76,7 @@ def avgVGRvsNProcessors():
                  'yaxis_title': "Average VGR Rating",
                  'xaxis_title': "Number of Processors",
                  'categories': [],
-                 'series-type': "single",
+                 'series_type': "single",
                  'values': []}
     distinct_number_dict = MachineInfo.objects.values('processors').distinct()
     for number in distinct_number_dict:
@@ -98,7 +98,7 @@ def logVGRvsProcessorFamily():
                  'xaxis_title': "Processor Family",
                  'yaxis_title': "Logarithmic VGR Rating",
                  'categories': [],
-                 'series-type': "single",
+                 'series_type': "single",
                  'values': []}
 
     distinct_categories_dict = MachineInfo.objects.values('vendor_id').distinct()
@@ -120,7 +120,7 @@ def logVGRvsOSType():
                  'yaxis_title': "Average VGR Rating",
                  'xaxis_title': "Operating System Type",
                  'categories': [],
-                 'series-type': "single",
+                 'series_type': "single",
                  'values': []}
     distinct_categories_dict = MachineInfo.objects.values('ostype').distinct()
     for category in distinct_categories_dict:
@@ -141,7 +141,7 @@ def logVGRvsNCores():
                  'yaxis_title': "Logarithmic VGR Rating",
                  'xaxis_title': "Number of CPUs",
                  'categories': [],
-                 'series-type': "single",
+                 'series_type': "single",
                  'values': []}
     distinct_number_dict = MachineInfo.objects.values('cores').distinct()
     for number in distinct_number_dict:
@@ -162,7 +162,7 @@ def runningTimevsProcessorFamily():
                  'xaxis_title': "Processor Family",
                  'yaxis_title': "Running Time",
                  'categories': [],
-                 'series-type': "single",
+                 'series_type': "single",
                  'values': []}
     distinct_categories_dict = MachineInfo.objects.values('vendor_id').distinct()
     for category in distinct_categories_dict:
@@ -179,7 +179,7 @@ def absPerformancevsRefImages():
                  'chart_type': "bar",
                  'xaxis_title': "Image",
                  'yaxis_title': "Absolute Rays per Sec",
-                 'series-type': "single",
+                 'series_type': "single",
                  'categories': ["Moss", "World", "Star", "Bldg391", "M35", "Sphflake"],
                  'values': []}
     for category in data_dict['categories']:
@@ -187,6 +187,34 @@ def absPerformancevsRefImages():
         model_class = get_model('plots', model_name)
         value_pair = [category, model_class.objects.all().aggregate(Avg('abs_rps'))['abs_rps__avg']]
         data_dict['values'].append(value_pair)
+    return data_dict
+
+
+def processorFamiliesvsRefImages():
+    """
+    Returns the aggregated data for the performance of Processor Families against Reference Images plot
+    in the form of a dictionary.
+    """
+    data_dict = {'chart_title': "Absolute Rays Per Sec against Reference Images",
+                 'chart_type': "line",
+                 'xaxis_title': "Image",
+                 'yaxis_title': "Absolute Rays per Sec",
+                 'series_type': "multi",
+                 'categories': ["Moss", "World", "Star", "Bldg391", "M35", "Sphflake"],
+                 'labels': [],
+                 'values': []}
+    processor_dict = MachineInfo.objects.values('vendor_id').distinct()
+    series_data = []
+    for processor in processor_dict:
+        for category in data_dict['categories']:
+            model_name = 'Rt'+category
+            field = model_name.lower()
+            value_pair = [category,
+                          BenchmarkLogs.objects.filter(machineinfo__vendor_id=processor['vendor_id'])
+                              .aggregate(Avg(field+'__abs_rps'))[field+'__abs_rps__avg']]
+            series_data.append(value_pair)
+        data_dict['labels'].append(processor['vendor_id'])
+        data_dict['values'].append(series_data)
     return data_dict
 
 
@@ -199,7 +227,7 @@ def avgVGRvsCPUmhz():
                  'yaxis_title': "Average VGR Rating",
                  'xaxis_title': "CPU MHz",
                  'categories': [],
-                 'series-type': "single",
+                 'series_type': "single",
                  'values': []}
     distinct_number_dict = MachineInfo.objects.values('cpu_mhz').distinct()
     for number in distinct_number_dict:
