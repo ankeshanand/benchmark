dd6f82c24afc24470a1f85b7a73bbe8623e8def0
ankeshanand <ankeshanand1994@gmail.com>
2014-07-27 23:13:29 +0530

    Minor fixes

--- plots/urls.py
+++ plots/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 #from plots.views import rawdata, draw
 
 urlpatterns = patterns('',
-    url(r'/^(?P<plotname>[A-z]+)/$', views.draw, name='drawChart'),
-    url(r'/^(?P<plotname>[A-z]+)/data/$', views.rawdata, name='rawdata'),
-    url(r'/^$', TemplateView.as_view(template_name='index.html'))
+    url(r'^(?P<plotname>[A-z]+)/$', views.draw, name='drawChart'),
+    url(r'^(?P<plotname>[A-z]+)/data/$', views.rawdata, name='rawdata'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'))
 )
