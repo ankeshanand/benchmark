3e55c3d8dfbcd6ce9065c8f43de52a0845522daa
ankeshanand <ankeshanand1994@gmail.com>
2014-08-12 17:19:59 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,12 @@ import os
 import sys
 import site
 
-site.addsitedir('~/.virtualenvs/benchmarkenv/local/lib/python2.7/site-packages')
+site.addsitedir('/home/benchmark/deployment/benchmark/benchmarkenv/local/lib/python2.7/site-packages')
 sys.path.append('/home/benchmark/deployment')
 sys.path.append('/home/benchmark/deployment/benchmark')
 
 # Activate your virtual env
-activate_env = os.path.expanduser('~/.virtualenvs/benchmarkenv/bin/activate_this.py')
+activate_env = os.path.expanduser('/home/benchmark/deployment/benchmark/benchmarkenv/bin/activate_this.py')
 execfile(activate_env, dict(__file__=activate_env))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'benchmark.settings'
 
